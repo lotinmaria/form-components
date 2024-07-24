8db7c73ec7e9a06d8e3984eaee6d154e57ccc914
lotinmaria <mariaholotin2007@gmail.com>
2024-07-24 11:59:19 -0300

    criando componente de inpu e form

---
+++ src/components/form.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Input from './input.vue'
+
+function save() {
+    
+}
+</script>
+
+<template>
+    <form :action="save">
+        <Input type="text" required="required" maxlength="55"/>
+    </form>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/input.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+const info = defineProps({
+    type: {
+        type: String,
+        default: "text",
+    },
+    default: {
+        type: null,
+        default: ""
+    },
+    required: {
+        type: String,
+        default: "",
+    },
+    min_length: {
+        type: Number,
+    },
+    min: {
+        type: Number,
+    },
+    maxlength: {
+        type: Number,
+    }
+
+})
+</script>
+
+<template>
+    <input :type="info.type" :default="info.default" info.required :min_length="info.min_length" :min="info.min">
+</template>
+
+<style></style>
